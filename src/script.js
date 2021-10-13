4aae44d99de79e6eeee71969a56b8bf0849f2795
KarenStewart1 <karenstewart1117@gmail.com>
2021-10-13 17:44:34 +0200

    styling feature button when clicked

--- src/script.js
+++ src/script.js
@@ -1,6 +1,18 @@
-
 let drumButtons = document.querySelectorAll("button");
 
 drumButtons.forEach(function (button) {
   button.addEventListener("click", function () {
+    button.classList.add("active-button");
+    setTimeout(function () {
+      button.classList.remove("active-button");
+    }, 50);
+  });
+});
+
+let audio = document.querySelectorAll("audio");
+audio.forEach(function (audio) {
+  audio.classList.add("active-audio");
+  setTimeout(function () {
+    audio.classList.remove("active-audio");
+  }, 80);
 });
